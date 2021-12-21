5b43a96aaa42a2ffc480f6802db0adf3d963516a
Azule2 <69428406+Azule2@users.noreply.github.com>
2021-12-21 23:42:14 +0300

    main.dart
    new Course Dart Flutter

---
+++ main.dart
@@ -0,0 +1,40 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+home:Scaffold(
+    appBar: AppBar(
+
+     title: const Text("IMine"),
+
+      centerTitle: true,
+     backgroundColor: Colors.deepPurple[900],
+    ),
+    body:  const Center(
+
+      child: Text("Hello IMine",
+      style: TextStyle(
+        fontSize: 24.0,
+        color:Colors.deepPurpleAccent,
+        fontWeight: FontWeight.bold,
+        letterSpacing: 2.4,
+        fontFamily: 'Hollo',
+
+      ),
+      ),
+      
+    ),
+    floatingActionButton: FloatingActionButton(
+      child: Text('Click me'),
+      backgroundColor: Colors.blue, onPressed: () { 
+        
+       },
+ 
+    ),
+   ) 
+  ));
+}
+
+ 
